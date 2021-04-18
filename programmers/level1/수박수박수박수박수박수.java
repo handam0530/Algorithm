65beb7d0092924568929e1d1ae1d2daf7e0b1d13
handam0530 <hse0530@gmail.com>
2021-04-18 23:21:55 +0900

    Create 수박수박수박수박수박수.java

---
+++ programmers/level1/수박수박수박수박수박수.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class 수박수박수박수박수박수 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+    }
+
+    private static String solution(int n){
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+             if(i % 2 == 0) {
+                 sb.append("수");
+             } else {
+                 sb.append("박");
+             }
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
